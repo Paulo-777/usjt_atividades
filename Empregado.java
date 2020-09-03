d931d15829b079897a5e2fbaf0994f8ffa46ac47
Paulo Santos <paulosantos.3309@aluno.saojudas.br>
2020-09-03 20:36:25 -0300

    Calculo de salario efetuado de acordo com o tipo

--- Empregado.java
+++ Empregado.java
@@ -1,6 +1,11 @@
 public class Empregado{
     private String nome;
     private int idade;
+    private int tipo;
+    private double salario;
+    private double comissao;
+    private double bonus;
+    
     public String getNome() {
         return nome;
 }
